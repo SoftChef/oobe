83d485cad13d4211e12190cda009890071e0db0b
zhihao <zhihao@rd01de-MacBook-Pro.local>
2019-11-19 13:24:00 +0800

    fix lint

--- src/Helper.js
+++ src/Helper.js
@@ -197,7 +197,7 @@ class Helper {
         if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
             now += performance.now()
         }
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
             var r = (now + Math.random() * 16) % 16 | 0
             now = Math.floor(now / 16)
             return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16)
